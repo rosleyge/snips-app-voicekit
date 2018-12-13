1aab872f3933e4de77eec9b249a9e37b19875d94
rosleyge <45656636+rosleyge@users.noreply.github.com>
2018-12-13 15:29:47 +0800

    Update action-app_voicekit.py

--- action-app_voicekit.py
+++ action-app_voicekit.py
@@ -72,7 +72,8 @@ class VoiceKit(object):
         time.sleep(1.5)
         self.relay1.on()
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Party begin    play some music", "")     
+        hermes.publish_start_session_notification(intent_message.site_id, "Party begin, Happy together", "")    
+        hermes.publish_start_session_notification(intent_message.site_id, "Hey google, play some popular songs", "") 
         
     def takebreak(self, hermes, intent_message):
         # terminate the session first if not continue
